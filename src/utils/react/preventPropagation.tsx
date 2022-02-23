9b8f8cee45a007e629113f4d35641056c69810de
nia <vladimir.serg@bk.ru>
2022-02-23 18:31:19 +0300

    Правки по utils

--- src/utils/react/preventPropagation.tsx
+++ src/utils/react/preventPropagation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function preventPropagation<T extends (e: any) => void>(fn: T) {
+export default function preventPropagation<T extends (e: any) => void>(fn: T) {
     return <E extends React.SyntheticEvent<any>>(e: E) => {
         e.stopPropagation();
         fn(e);
